d264216e296cb19d1e533fe4402bc7e2c6d4c7ec
ianreas <muhammedik111101@gmail.com>
2022-10-28 18:36:36 -0400

    adding analytics 2nd attempt

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
-import {Analytics} from "@vercel/analytics"
+import {Analytics} from "@vercel/analytics/react"
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
