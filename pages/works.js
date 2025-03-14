2f9de806037b14c7f13284f1f9b5f156c0686ab8
ianreas <muhammedik111101@gmail.com>
2025-03-13 20:26:52 -0400

    m

--- pages/works.js
+++ pages/works.js
@@ -1,14 +1,10 @@
-import { Container, Heading, SimpleGrid} from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
-import thumbBuildingAlpha from "../public/iv_surface_new.png"
-import thumbRecipeBook from "../public/recipe1.png"
-import thumbWeather from "../public/weather.png"
-import thumbRaushan from "../public/raushan.png"
+import thumbBuildingAlpha from '../public/iv_surface_new.png'
+import thumbMeowPaas from '../public/meowpaas.png'
 import thumbGame from '../public/game.png'
-
-
 
 const Works = () => (
   <Layout title="Works">
@@ -19,32 +15,23 @@ const Works = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="" title="BuildingAlpha" thumbnail={thumbBuildingAlpha}>
-            BuildingAlpha is a single, integrated platform to search, retrieve, visualize and analyze financial options data.
-          </WorkGridItem>
-        </Section>
-        <Section>
           <WorkGridItem
             id=""
-            title="Foodnote"
-            thumbnail={thumbRecipeBook}
+            title="BuildingAlpha"
+            thumbnail={thumbBuildingAlpha}
           >
-            A website where people can post, share, and discover new recipes, cooks, images, videos and how-to&apos;s based on the food they like and the friends they follow.
+            BuildingAlpha is a single, integrated platform to search, retrieve,
+            visualize and analyze financial options data.
           </WorkGridItem>
         </Section>
 
         <Section delay={0.1}>
-          <WorkGridItem
-            id=""
-            title="RaushanLoads"
-            thumbnail={thumbRaushan}
-          >
-            A freight shipping services platform where people and businesses can work with logistical companies to arrange the delivery of loads.
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem id="" thumbnail={thumbWeather} title="Weatherio">
-            A simple weather website that does it&apos;s job.
+          <WorkGridItem id="" title="MeowPaas" thumbnail={thumbMeowPaas}>
+            A cloud deployment platform with enterprise-grade performance
+            optimization built into its core. Built on top of EC2, it uses
+            Kubernetes to deploy applications that require container-level
+            optimizations to achieve maximum performance for different
+            workloads.
           </WorkGridItem>
         </Section>
       </SimpleGrid>
@@ -53,13 +40,16 @@ const Works = () => (
       </Heading>
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="" title="The Ode of the Times" thumbnail={thumbGame}>
-            An RPG, quest based game. Collaborated with Meta software engineer Yermek Ibrayev. Work still in progress.
+          <WorkGridItem
+            id=""
+            title="The Ode of the Times"
+            thumbnail={thumbGame}
+          >
+            An RPG, quest based game. Collaborated with Meta software engineer
+            Yermek Ibrayev. Work still in progress.
           </WorkGridItem>
         </Section>
-        
-        </SimpleGrid>
-      
+      </SimpleGrid>
     </Container>
   </Layout>
 )
